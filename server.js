417b3ad07fa1012fb5ca1d29b097bb9911460b0c
박현재/NE <hyeonjae.park@nhnent.com>
2015-07-09 19:37:46 +0900

    post기능 분리 및 router 사용

--- server.js
+++ server.js
@@ -1,37 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var Post = require('./javascript/post');
 
 var app = express();
 app.use(bodyParser.json());
 
+app.use('/api/posts', require('./controllers/api/posts'));
+
 app.get('/', function(req, res) {
 	res.sendfile('layouts/index.html');
 });
-
-app.get('/api/posts', function(req, res, next) {
-	Post.find(function(err, posts) {
-		if (err) {
-			return next(err);
-		}
-		res.json(posts);
-	});
-});
-
-app.post('/api/posts', function(req, res) {
-	var post = new Post({
-		username: req.body.username,
-		body: req.body.body
-	});
-
-	post.save(function(err, post) {
-		if (err) {
-			return next(err)
-		}
-		res.json(201, post);
-	});
-});
-
 
 app.listen(3000, function () {
 	console.log('Server listening on', 3000);
