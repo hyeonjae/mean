7921019b937cc9da62a9a0911e372527f05b4553
박현재/NE <hyeonjae.park@nhnent.com>
2015-07-13 21:34:45 +0900

    revert heroku

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://heroku_dx58dctv:usa16eoq51vu2ksul4vsn34mal@ds047782.mongolab.com:47782/heroku_dx58dctv', function () {
-//mongoose.connect('mongodb://localhost/social', function () {
+mongoose.connect('mongodb://localhost/social', function () {
 	console.log('mongodb connected');
 });
 
